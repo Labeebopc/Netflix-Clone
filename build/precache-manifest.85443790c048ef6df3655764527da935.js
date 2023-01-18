5304a584082bdd10395d238e12ad3e7d4dfd09d0
Labeebopc <Labeebopc313@gmail.com>
2023-01-19 00:02:54 +0530

    build file added

---
+++ build/precache-manifest.85443790c048ef6df3655764527da935.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "945a091e6d69318bdeb3",
+    "url": "/static/css/main.7b320015.chunk.css"
+  },
+  {
+    "revision": "945a091e6d69318bdeb3",
+    "url": "/static/js/main.945a091e.chunk.js"
+  },
+  {
+    "revision": "7d660da11055cc7b206c",
+    "url": "/static/js/1.7d660da1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c87c938b610655bbaeafe1df2156549c",
+    "url": "/index.html"
+  }
+];
